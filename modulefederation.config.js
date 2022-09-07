9da9af46cecf14281bbb353b2201c676b3e87c00
triazur <guillaume.cassin@triazur.com>
2022-09-07 08:02:11 +0200

    update module federation

--- modulefederation.config.js
+++ modulefederation.config.js
@@ -1,20 +1,20 @@
 const { dependencies } = require('./package.json');
 
 module.exports = {
-    name: 'webkit',
-    exposes: {
-        './App': './src/App',
+  name: 'shearAndTensionBoltsEurocodes',
+  exposes: {
+    './App': './src/App',
+  },
+  filename: 'remoteEntry.js',
+  shared: {
+    ...dependencies,
+    react: {
+      singleton: true,
+      requiredVersion: dependencies['react'],
     },
-    filename: 'remoteEntry.js',
-    shared: {
-        ...dependencies,
-        react: {
-            singleton: true,
-            requiredVersion: dependencies['react'],
-        },
-        'react-dom': {
-            singleton: true,
-            requiredVersion: dependencies['react-dom'],
-        },
+    'react-dom': {
+      singleton: true,
+      requiredVersion: dependencies['react-dom'],
     },
+  },
 };
